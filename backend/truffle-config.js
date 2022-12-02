50fab72190e1fe6ebd87234a52dcd4038838932a
Kennedy Eruba <erubakenny@gmail.com>
2022-12-02 20:06:27 +0000

    WIP

---
+++ backend/truffle-config.js
@@ -0,0 +1,10 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const fs = require('fs');
+module.exports = {
+  mocha: {},
+  compilers: {
+    solc: {
+      version: '0.8.15'
+    }
+  }
+};
---
+++ utils/constants.js
@@ -0,0 +1,3 @@
+import abi from './Lottery.json'
+export const lotteryAbi = abi.abi
+export const LotteryAddress = '0x2186F0B96165d1a5EE0937477A1E6DD6F312f6d1'
